1a3734bfb66c0830b4f9f4cb977a92702547cd63
krisnachy <cahyayuana@gmail.com>
2021-02-03 06:07:41 +0700

    [Krisna] 1st

---
+++ index.js
@@ -0,0 +1,8 @@
+//index.js
+console.log("Hello World!")
+
+const os = require('os')
+const chalk = require('chalk')
+
+console.log('Free Memory: ', os.freemem());
+console.log(chalk.red('Hello World!'));
